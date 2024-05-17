8734006e4ae499211ebf4305afb4480a6398ace3
Zuzana-bor <z.kasikova@gmail.com>
2024-05-17 04:55:07 +0200

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
 import './App.css';
-import Header from './layout/Header';
-import Hero from './layout/Hero';
-import Offer from './layout/Offer';
-import Footer from './layout/Footer';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
 
 function App() {
   return (
     <div>
-      <Header />
-      <Hero />
-      <Offer />
-      <Footer />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Header from '../layout/Header';
+import Hero from '../layout/Hero';
+import Offer from '../layout/Offer';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Offer />
+    </>
+  );
+};
+
+export default Home;
