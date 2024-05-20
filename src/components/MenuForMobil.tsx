2b0813e9cf0afe61ec4d2daf4c3f7ddd97c18062
Zuzana-bor <z.kasikova@gmail.com>
2024-05-20 12:48:14 +0200

    wip

--- src/components/MenuForMobil.tsx
+++ src/components/MenuForMobil.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const MenuForMobil = () => {
   return (
-    <div className=" absolute top-5 left-0   z-10 transition-transform duration-1000 ease-in-out border w-96 z-5 bg-gray-100 ">
+    <div className="absolute top-5 left-0 z-10 transition-transform  ease-linear border w-96 bg-gray-100">
       <ul>
         <div>logo</div>
         <nav>
